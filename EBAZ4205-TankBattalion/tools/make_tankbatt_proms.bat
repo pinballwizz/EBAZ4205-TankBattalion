5c0d3f50c18e336aca7d7067da98cd353a3f3319
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2026-05-27 11:53:50 +1000

    Add files via upload

---
+++ EBAZ4205-TankBattalion/tools/make_tankbatt_proms.bat
@@ -0,0 +1,8 @@
+make_vhdl_prom tb1-1.1a ROM_A1.vhd
+make_vhdl_prom tb1-2.1b ROM_B1.vhd
+make_vhdl_prom tb1-3.1c ROM_C1.vhd
+make_vhdl_prom tb1-4.1d ROM_D1.vhd
+make_vhdl_prom tb1-5.2k ROM_K2.vhd
+make_vhdl_prom bct1-1.l3 ROM_L3.vhd
+
+pause
